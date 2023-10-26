fe53988ef3d38dc6c74b4a4355545d5ddf2af176
jbrouwerdigibase <jbrouwer@digibase.nl>
2023-10-26 14:00:56 +0200

    Add bSDD search page

--- BsddRevitPlugin.Logic/View/bSDDPanel.xaml.cs
+++ BsddRevitPlugin.Logic/View/bSDDPanel.xaml.cs
@@ -5,64 +5,71 @@ using System.Windows.Data;
 using Autodesk.Revit.UI;
 using BsddRevitPlugin.Logic.Model;
 using BsddRevitPlugin.Logic.ViewModel;
-using System.Windows.Input;
 using ComboBox = System.Windows.Controls.ComboBox;
 using System.ComponentModel;
 
+/// <summary>
+/// Event handler for the selection method combo box. Clears the element manager and raises the appropriate external event based on the selected item in the combo box.
+/// </summary>
+/// <param name="sender">The selection method combo box.</param>
+/// <param name="e">The selection changed event arguments.</param>
 namespace BsddRevitPlugin.Logic.View
 {
-    /// <summary>
-    /// Interaction logic for UserControl1.xaml
-    /// </summary>
+    // This class represents the main panel of the bSDD Revit plugin
     public partial class bSDDPanel : Page, IDockablePaneProvider
     {
-
-        //Declaration
+        // Declaration of events and external events
         BSDDconnect.EventMakeSelection SelectEHMS;
         BSDDconnect.EventSelectAll SelectEHSA;
         BSDDconnect.EventSelectView SelectEHSV;
         ExternalEvent SelectEEMS, SelectEESA, SelectEESV;
 
-        #region Data
+        // Data fields
         private Guid m_targetGuid = new Guid("D7C963CE-B3CA-426A-8D51-6E8254D21158");
         private DockPosition m_position = DockPosition.Floating;
         private int m_left = 100;
         private int m_right = 100;
         private int m_top = 100;
         private int m_bottom = 100;
-        #endregion
 
-        // constructor
-        public bSDDPanel()
+        // Constructor
+        public bSDDPanel(string addinLocation)
         {
             InitializeComponent();
 
+            // Set the address of the CefSharp browser component to the index.html file of the plugin
+            Browser.Address = addinLocation + "/html/index.html";
+            Browser.JavascriptObjectRepository.Register("bsddBridge", new BsddBridge(), true);
+
+            // Set the data context of the panel to an instance of ElementViewModel
             ElementViewModel elementViewModel = new ElementViewModel();
             this.DataContext = elementViewModel;
             lbxSelection.ItemsSource = elementViewModel.Elems;
 
-            ////Sorteren van elementenlijst
+            // Sort the list of elements by category, family, and type
             CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(lbxSelection.ItemsSource);
             PropertyGroupDescription groupDescription = new PropertyGroupDescription("Category");
             view.GroupDescriptions.Add(groupDescription);
             view.SortDescriptions.Add(new SortDescription("Family", ListSortDirection.Ascending));
             view.SortDescriptions.Add(new SortDescription("Type", ListSortDirection.Ascending));
-            
-            ////initialazor
+
+            // Initialize the events and external events
             SelectEHMS = new BSDDconnect.EventMakeSelection();
             SelectEHSA = new BSDDconnect.EventSelectAll();
             SelectEHSV = new BSDDconnect.EventSelectView();
             SelectEEMS = ExternalEvent.Create(SelectEHMS);
             SelectEESA = ExternalEvent.Create(SelectEHSA);
             SelectEESV = ExternalEvent.Create(SelectEHSV);
-            
+
+            // Add the selection methods to the selection method combo box
             SM.Items.Add(new ComboBoxItem() { Content = "Selection method:", IsSelected = true, IsEnabled = false });
             SM.Items.Add(new ComboBoxItem() { Content = "Make selection" });
             SM.Items.Add(new ComboBoxItem() { Content = "Select all" });
-            SM.Items.Add(new ComboBoxItem() { Content = "Select visable in view" });
+            SM.Items.Add(new ComboBoxItem() { Content = "Select visible in view" });
             SM.SelectedItem = SM.Items[0];
         }
 
+        // Implement the IDockablePaneProvider interface
         public void SetupDockablePane(Autodesk.Revit.UI.DockablePaneProviderData data)
         {
             data.FrameworkElement = this as FrameworkElement;
@@ -80,13 +87,14 @@ namespace BsddRevitPlugin.Logic.View
             if (m_position == DockPosition.Tabbed)
             {
                 data.InitialState.TabBehind = Autodesk.Revit.UI.DockablePanes.BuiltInDockablePanes.ViewBrowser;
-
             }
             if (m_position == DockPosition.Floating)
             {
                 data.InitialState.SetFloatingRectangle(new Autodesk.Revit.DB.Rectangle(0, 0, 100, 710));
             }
         }
+
+        // Set the initial docking parameters of the panel
         public void SetInitialDockingParameters(int left, int right, int top, int bottom, DockPosition position, Guid targetGuid)
         {
             m_position = position;
@@ -97,36 +105,39 @@ namespace BsddRevitPlugin.Logic.View
             m_targetGuid = targetGuid;
         }
 
+        // Event handlers
         private void PaneInfoButton_Click(object sender, RoutedEventArgs e)
         {
-
+            // TODO: Implement this method
         }
 
         private void wpf_stats_Click(object sender, RoutedEventArgs e)
         {
-
+            // TODO: Implement this method
         }
 
         private void btn_getById_Click(object sender, RoutedEventArgs e)
         {
-
+            // TODO: Implement this method
         }
 
         private void btn_listTabs_Click(object sender, RoutedEventArgs e)
         {
-
+            // TODO: Implement this method
         }
 
         private void DockableDialogs_Loaded(object sender, RoutedEventArgs e)
         {
-
+            // TODO: Implement this method
         }
 
+        // Event handler for the selection method combo box
         private void SM_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            // Clear the element manager
             ElemManager.Clear();
 
-            //Make the IExternalcommand happen
+            // Raise the appropriate external event based on the selected item in the combo box
             if (((ComboBoxItem)(((ComboBox)sender).SelectedItem)).Content.ToString() == "Make selection")
             {
                 SelectEEMS.Raise();
@@ -135,7 +146,7 @@ namespace BsddRevitPlugin.Logic.View
             {
                 SelectEESA.Raise();
             }
-            else if (((ComboBoxItem)(((ComboBox)sender).SelectedItem)).Content.ToString() == "Select visable in view")
+            else if (((ComboBoxItem)(((ComboBox)sender).SelectedItem)).Content.ToString() == "Select visible in view")
             {
                 SelectEESV.Raise();
             };
