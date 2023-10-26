d1b8cfb5b706f9150bac681601cf489f8ae83513
jbrouwerdigibase <jbrouwer@digibase.nl>
2023-10-26 14:00:56 +0200

    Add bSDD search page

--- BsddRevitPlugin.Common/Startup.cs
+++ BsddRevitPlugin.Common/Startup.cs
@@ -1,56 +1,78 @@
 ﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.UI;
-using BsddRevitPlugin.Logic.DockablePanel;
 using BsddRevitPlugin.Logic.View;
 using BsddRevitPlugin.Resources;
 using System;
 using System.Diagnostics;
 using System.IO;
 using System.Reflection;
-using System.Windows;
 using System.Windows.Media.Imaging;
-using UIFramework;
 
 namespace BsddRevitPlugin.Common
 {
+    /// <summary>
+    /// This class is responsible for starting up the Revit plugin.
+    /// </summary>
     [Transaction(TransactionMode.Manual)]
     [Regeneration(RegenerationOption.Manual)]
     public class Startup : IExternalApplication
     {
+        /// <summary>
+        /// This method is called when the add-in is shut down.
+        /// </summary>
+        /// <param name="application">The UIControlledApplication object representing the Revit application.</param>
+        /// <returns>A Result object indicating whether the shutdown was successful.</returns>
         public Result OnShutdown(UIControlledApplication application)
         {
             return Result.Succeeded;
         }
 
+        /// <summary>
+        /// This method is called when the add-in is started up. It registers a dockable panel for the Revit project and adds ribbon buttons to the UI.
+        /// </summary>
+        /// <param name="application">The UIControlledApplication object representing the Revit application.</param>
+        /// <returns>A Result object indicating whether the startup was successful.</returns>
         public Result OnStartup(UIControlledApplication application)
         {
-            //Registreer Dockable panel voor Revit project geopend wordt.
-            RegisterDockPanel(application);
+            // Get the current add-in location
+            string addinLocation = Assembly.GetExecutingAssembly().Location;
+            string addinDirectory = Path.GetDirectoryName(addinLocation);
 
+            // Register Dockable panel for Revit project when it is opened.
+            RegisterDockPanel(application, addinDirectory);
+
+            // Add ribbon buttons to the UI.
             AddRibbonButtons(application);
+
+            // Open logs.
             Main.Instance.OpenLogs();
 
             return Result.Succeeded;
         }
 
-        BitmapImage NewBitmapImage(
-            Assembly a, string imageName)
-        {
-            Stream s = a.GetManifestResourceStream(imageName);
-            BitmapImage img = new BitmapImage();
-            img.BeginInit();
-            img.StreamSource = s;
-            img.EndInit();
-            return img;
-        }
+        // BitmapImage NewBitmapImage(
+        //     Assembly a, string imageName)
+        // {
+        //     Stream s = a.GetManifestResourceStream(imageName);
+        //     BitmapImage img = new BitmapImage();
+        //     img.BeginInit();
+        //     img.StreamSource = s;
+        //     img.EndInit();
+        //     return img;
+        // }
 
-        private System.Windows.Media.ImageSource BmpImageSource(string embeddedPath)
-        {
-            Stream stream = this.GetType().Assembly.GetManifestResourceStream(embeddedPath);
-            var decoder = new BmpBitmapDecoder(stream, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.Default);
-            return decoder.Frames[0];
-        }
+        // private System.Windows.Media.ImageSource BmpImageSource(string embeddedPath)
+        // {
+        //     Stream stream = this.GetType().Assembly.GetManifestResourceStream(embeddedPath);
+        //     var decoder = new BmpBitmapDecoder(stream, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.Default);
+        //     return decoder.Frames[0];
+        // }
 
+
+        /// <summary>
+        /// This method adds ribbon buttons to the Revit UI.
+        /// </summary>
+        /// <param name="application">The UIControlledApplication object representing the Revit application.</param>
         private void AddRibbonButtons(UIControlledApplication application)
         {
             Assembly assembly = Assembly.GetExecutingAssembly();
@@ -69,31 +91,39 @@ namespace BsddRevitPlugin.Common
                 // tab already exists
             }
 
+            // Create the main button.
             PushButtonData pbd = new PushButtonData("Sample", "Click Me", executingAssemblyPath, "BsddRevitPlugin.Common.Commands.Popup");
             PushButtonData pbddp = new PushButtonData("Show/Hide", "Show/Hide selector", executingAssemblyPath, "DockablePanel.ShowDockableWindow");
             RibbonPanel panel = application.CreateRibbonPanel(eTabName, "bSDD");
-
-            // Create the main button.
             PushButton pb = panel.AddItem(pbd) as PushButton;
             PushButton pbdp = panel.AddItem(pbddp) as PushButton;
 
+            // Set button tooltips.
             pb.ToolTip = "This is a sample Revit button";
             pbdp.ToolTip = "Show/hide bSDD selection panel";
+
+            // Set button images.
             pb.LargeImage = ResourceImage.GetIcon("bsdd-label.png");
             pbdp.LargeImage = ResourceImage.GetIcon("bsdd-label.png");
         }
 
         #region DockablePanel
 
-        private void RegisterDockPanel(UIControlledApplication app)
+        /// <summary>
+        /// This method registers a dockable panel for the Revit project.
+        /// </summary>
+        /// <param name="app">The UIControlledApplication object representing the Revit application.</param>
+        /// <param name="addinLocation">The location of the add-in.</param>
+        private void RegisterDockPanel(UIControlledApplication app, string addinLocation)
         {
-            //Koppel het window aan de Mainpage
-            bSDDPanel MainDockableWindow = new bSDDPanel();
+            // Create a new bSDDPanel object and link it to the main window.
+            bSDDPanel MainDockableWindow = new bSDDPanel(addinLocation);
             DockablePaneProviderData data = new DockablePaneProviderData();
 
-            //Maak een ID aan
+            // Create a new DockablePane Id.
             DockablePaneId dpid = new DockablePaneId(new Guid("D7C963CE-B3CA-426A-8D51-6E8254D21158"));
 
+            // Register the dockable panel.
             app.RegisterDockablePane(dpid, "bSDD", MainDockableWindow as IDockablePaneProvider);
         }
 
