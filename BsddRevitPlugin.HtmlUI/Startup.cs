4cf32025c65d258b8ea1dc68e92c47bc2704a160
jbrouwerdigibase <jbrouwer@digibase.nl>
2023-11-02 13:21:59 +0100

    Add HtmlUi

---
+++ BsddRevitPlugin.HtmlUI/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace BsddRevitPlugin.HtmlUI
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        }
+    }
+}
