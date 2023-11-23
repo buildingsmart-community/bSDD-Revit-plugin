c1983147fc931908d5bc4706211a0c483739ab24
Rien <RvdScheur@DigiBase.nl>
2023-11-23 16:55:28 +0100

    reviteventwrappers

--- BsddRevitPlugin.Logic/UI/BsddBridge/BsddBridge.cs
+++ BsddRevitPlugin.Logic/UI/BsddBridge/BsddBridge.cs
@@ -1,6 +1,12 @@
-﻿using Autodesk.Revit.UI;
+﻿using Autodesk.Revit.DB.Events;
+using Autodesk.Revit.UI;
 using BsddRevitPlugin.Logic.IfcJson;
+using BsddRevitPlugin.Logic.UI.View;
+using BsddRevitPlugin.Logic.UI.Wrappers;
+using CefSharp;
 using Newtonsoft.Json;
+using System.IO;
+using System.Reflection;
 using System.Windows;
 using BSDDconnect = BsddRevitPlugin.Logic.UI.Wrappers;
 
@@ -13,13 +19,16 @@ namespace BsddRevitPlugin.Logic.UI.BsddBridge
     {
 
         // Declaration of events and external events
-        BSDDconnect.EventTest2 testEvent2;
-        ExternalEvent testExEvent2;
+        BSDDconnect.EventHandlerBsddSearch EventHandlerBsddSearch;
+        ExternalEvent ExEventBsddSearch;
+
         public BsddBridge()
         {
             // Initialize the events and external events
-            testEvent2 = new BSDDconnect.EventTest2();
-            testExEvent2 = ExternalEvent.Create(testEvent2);
+            //EventHandlerBsddSearch = EventHandlerBsddSearchUI;
+            EventHandlerBsddSearch = new EventHandlerBsddSearch();
+            ExEventBsddSearch = ExternalEvent.Create(EventHandlerBsddSearch);
+
         }
 
         /// <summary>
@@ -30,8 +39,12 @@ namespace BsddRevitPlugin.Logic.UI.BsddBridge
         public string Save(string ifcJsonData)
         {
 
+            //OpenBsddSearchUiCommand a = new OpenBsddSearchUiCommand();
 
-            testExEvent2.Raise();
+            //a.Execute();
+            //testExEvent2.Raise();
+           // EventHandlerBsddSearch.Execute(Ap);
+
             // Create an instance of the IfcDataConverter class
             var converter = new IfcJsonConverter();
 
@@ -45,6 +58,9 @@ namespace BsddRevitPlugin.Logic.UI.BsddBridge
 
         }
 
+
+        
+
         /// <summary>
         /// Opens the bSDD Search panel with the selected object parameters
         /// </summary>
@@ -52,8 +68,16 @@ namespace BsddRevitPlugin.Logic.UI.BsddBridge
         /// <returns></returns>
         public string bsddSearch(string ifcJsonData)
         {
+      //      uicapp.ControlledApplication
+      //.ApplicationInitialized
+      //  += ControlledApplication_ApplicationInitialized;
 
-            testExEvent2.Raise();
+            //uicapp.ControlledApplication.ApplicationInitialized += ControlledApplication_ApplicationInitialized;
+            //var command = new OpenBsddSearchUiCommand();
+            //command.Execute();
+
+            ExEventBsddSearch.Raise();
+
             // Create an instance of the IfcDataConverter class
             var converter = new IfcJsonConverter();
 
@@ -64,6 +88,19 @@ namespace BsddRevitPlugin.Logic.UI.BsddBridge
             // Return the serialized JSON data for the IfcData object
             return JsonConvert.SerializeObject(ifcData);
 
+        }
+        private void ControlledApplication_ApplicationInitialized(
+            object sender,
+            ApplicationInitializedEventArgs e)
+        {
+            var command = new OpenBsddSearchUiCommand();
+
+            //I never remember if the sender is Application or UIApplication
+
+            if (sender is UIApplication)
+                command.Execute(sender as UIApplication);
+            else
+                command.Execute(new UIApplication(sender as Autodesk.Revit.ApplicationServices.Application));
         }
 
         public string Button_Click(string data)
