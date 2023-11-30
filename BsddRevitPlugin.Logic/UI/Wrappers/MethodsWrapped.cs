3b2cd5d0a79bedf434986940e3331ae3e3c8eec4
jbrouwerdigibase <jbrouwer@digibase.nl>
2023-11-30 12:05:40 +0100

    Fix selection without description

--- BsddRevitPlugin.Logic/UI/Wrappers/MethodsWrapped.cs
+++ BsddRevitPlugin.Logic/UI/Wrappers/MethodsWrapped.cs
@@ -1,4 +1,4 @@
-﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using BsddRevitPlugin.Logic.IfcJson;
@@ -18,69 +18,26 @@ using System.Windows.Threading;
 using static BsddRevitPlugin.Logic.Model.ElementsManager;
 using Document = Autodesk.Revit.DB.Document;
 
+/// <summary>
+/// Represents an abstract base class for handling Revit selection events.
+/// </summary>
 namespace BsddRevitPlugin.Logic.UI.Wrappers
 {
 
-    public class EventMakeSelection : RevitEventWrapper<string>
+    public abstract class EventRevitSelection : RevitEventWrapper<string>
     {
-
         Logger logger = LogManager.GetCurrentClassLogger();
 
         static List<Element> elemList = new List<Element>();
-        Select Selectorlist = new Select();
 
+        protected Select Selectorlist = new Select();
         ChromiumWebBrowser browser;
 
         public override void Execute(UIApplication uiapp, string args)
         {
             var uidoc = uiapp.ActiveUIDocument;
             var doc = uidoc.Document;
-
-            // Get all elements selected
-            elemList = Selectorlist.SelectElements(uiapp);
-
-            // Filter elements to remove non-element categories
-            elemList = ListFilter(elemList);
-
-            // Pack data into json format
-            BsddBridgeData selectionData = SelectionToJson(doc, elemList);
-
-            // Send MainData to BsddSelection html
-            UpdateBsddSelection(selectionData);
-
-        }
-        public void SetBrowser(ChromiumWebBrowser browserObject)
-        {
-            browser = browserObject;
-        }
-
-        private async void UpdateBsddSelection(BsddBridgeData ifcData)
-        {
-            var jsonString = JsonConvert.SerializeObject(ifcData);
-            var jsFunctionCall = $"updateSelection({jsonString});";
-
-            if (browser.IsBrowserInitialized)
-            {
-                browser.ExecuteScriptAsync(jsFunctionCall);
-            }
-        }
-    }
-
-    public class EventSelectAll : RevitEventWrapper<string>
-    {
-        Logger logger = LogManager.GetCurrentClassLogger();
-
-        static List<Element> elemList = new List<Element>();
-        Select Selectorlist = new Select();
-
-        ChromiumWebBrowser browser;
-        public override void Execute(UIApplication uiapp, string args)
-        {
-            var uidoc = uiapp.ActiveUIDocument;
-            var doc = uidoc.Document;
-
-            // Get all elements in project
-            elemList = Selectorlist.AllElements(uiapp);
+            elemList = GetSelection(uiapp);
 
             // Filter elements to remove non-element categories
             elemList = ListFilter(elemList);
@@ -91,52 +48,14 @@ namespace BsddRevitPlugin.Logic.UI.Wrappers
             // Send MainData to BsddSelection html
             UpdateBsddSelection(selectionData);
         }
+
+        protected abstract List<Element> GetSelection(UIApplication uiapp);
+
         public void SetBrowser(ChromiumWebBrowser browserObject)
         {
             browser = browserObject;
         }
-        private async void UpdateBsddSelection(BsddBridgeData ifcData)
-        {
-            var jsonString = JsonConvert.SerializeObject(ifcData);
-            var jsFunctionCall = $"updateSelection({jsonString});";
 
-            if (browser.IsBrowserInitialized)
-            {
-                browser.ExecuteScriptAsync(jsFunctionCall);
-            }
-        }
-    }
-
-    public class EventSelectView : RevitEventWrapper<string>
-    {
-        Logger logger = LogManager.GetCurrentClassLogger();
-
-        static List<Element> elemList = new List<Element>();
-
-        Select Selectorlist = new Select();
-        ChromiumWebBrowser browser;
-
-        public override void Execute(UIApplication uiapp, string args)
-        {
-            var uidoc = uiapp.ActiveUIDocument;
-            var doc = uidoc.Document;
-
-            // Get all elements in view
-            elemList = Selectorlist.AllElementsView(uiapp);
-
-            // Filter elements to remove non-element categories
-            elemList = ListFilter(elemList);
-
-            // Pack data into json format
-            BsddBridgeData selectionData = SelectionToJson(doc, elemList);
-
-            // Send MainData to BsddSelection html
-            UpdateBsddSelection(selectionData);
-        }
-        public void SetBrowser(ChromiumWebBrowser browserObject)
-        {
-            browser = browserObject;
-        }
         private async void UpdateBsddSelection(BsddBridgeData ifcData)
         {
             var settings = new JsonSerializerSettings
@@ -151,10 +70,45 @@ namespace BsddRevitPlugin.Logic.UI.Wrappers
                 browser.ExecuteScriptAsync(jsFunctionCall);
             }
         }
+    }
 
+    /// <summary>
+    /// Selection event for manualy selecting elements in Revit.
+    /// </summary>
+    public class EventMakeSelection : EventRevitSelection
+    {
+        protected override List<Element> GetSelection(UIApplication uiapp)
+        {
+            return Selectorlist.SelectElements(uiapp);
+        }
+    }
+
+    /// <summary>
+    /// Selection event for selecting all elements in Revit.
+    /// </summary>
+    public class EventSelectAll : EventRevitSelection
+    {
+        protected override List<Element> GetSelection(UIApplication uiapp)
+        {
+            return Selectorlist.AllElements(uiapp);
+        }
+    }
+
+    /// <summary>
+    /// Selection event for selecting all elements in the current Revit view.
+    /// </summary>
+    public class EventSelectView : EventRevitSelection
+    {
+        protected override List<Element> GetSelection(UIApplication uiapp)
+        {
+            return Selectorlist.AllElementsView(uiapp);
+        }
     }
 
 
+    /// <summary>
+    /// Represents a class that triggers the writing of IFC data into a Revit type object.
+    /// </summary>
     public class UpdateElementtypeWithIfcData : RevitEventWrapper<string>
     {
         Logger logger = LogManager.GetCurrentClassLogger();
@@ -175,8 +129,9 @@ namespace BsddRevitPlugin.Logic.UI.Wrappers
 
     }
 
-
-
+    /// <summary>
+    /// Represents an event handler for the BsddSearch event that is fired from the BsddSelection window.
+    /// </summary>
     public class EventHandlerBsddSearch : RevitEventWrapper<string>
     {
         Logger logger = LogManager.GetCurrentClassLogger();
