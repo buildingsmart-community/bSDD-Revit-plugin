35ab8c81cc7249458b51cf4d0d169fffb4d31a74
Erik Bouwens <ebouwens@heijmans.nl>
2024-04-17 11:15:04 +0200

    update 17-4

--- BsddRevitPlugin.Logic/UI/DockablePanel/Selectors.cs
+++ BsddRevitPlugin.Logic/UI/DockablePanel/Selectors.cs
@@ -29,6 +29,23 @@ namespace BsddRevitPlugin.Logic.UI.DockablePanel
             return list;
         }
 
+        public List<Element> AllElementsViewI(UIApplication uiapp)
+        {
+            //Select all elements in view
+            List<Element> list = new List<Element>();
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Autodesk.Revit.DB.Document doc = uidoc.Document;
+            FilteredElementCollector allElementsInView = new FilteredElementCollector(doc, doc.ActiveView.Id);
+            allElementsInView.WhereElementIsNotElementType().WhereElementIsViewIndependent();
+            IList elementsInView = (IList)allElementsInView.ToElements();
+            foreach (Element elem in elementsInView)
+            {
+                list.Add(doc.GetElement(elem.Id) as Element);
+            }
+
+            return list;
+        }
+
         public List<ElementType> AllElements(UIApplication uiapp)
         {
             //Select alle elementen in document
@@ -45,6 +62,22 @@ namespace BsddRevitPlugin.Logic.UI.DockablePanel
             return list;
         }
 
+        public List<Element> AllElementsI(UIApplication uiapp)
+        {
+            //Select alle elementen in document
+            List<Element> list = new List<Element>();
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Autodesk.Revit.DB.Document doc = uidoc.Document;
+            FilteredElementCollector allElements = new FilteredElementCollector(doc);
+            allElements.WhereElementIsNotElementType().WhereElementIsViewIndependent();
+            IList elementsAll = (IList)allElements.ToElements();
+            foreach (Element elem in elementsAll)
+            {
+                list.Add(doc.GetElement(elem.Id) as Element);
+            }
+            return list;
+        }
+
         public List<ElementType> SelectElements(UIApplication uiapp)
         {
             //Select by selection
@@ -56,6 +89,21 @@ namespace BsddRevitPlugin.Logic.UI.DockablePanel
             {
                 Element elem = doc.GetElement(element);
                 list.Add(doc.GetElement(elem.GetTypeId()) as ElementType);
+            }
+            return list;
+        }
+
+        public List<Element> SelectElementsI(UIApplication uiapp)
+        {
+            //Select by selection
+            List<Element> list = new List<Element>();
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Autodesk.Revit.DB.Document doc = uidoc.Document;
+            IList<Reference> collectionSelect = uidoc.Selection.PickObjects(ObjectType.Element);
+            foreach (Reference element in collectionSelect)
+            {
+                Element elem = doc.GetElement(element);
+                list.Add(doc.GetElement(elem.Id) as Element);
             }
             return list;
         }
