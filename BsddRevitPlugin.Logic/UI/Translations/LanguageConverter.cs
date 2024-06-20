fe534a38e0b672cda75895624c7fc52583ba321a
jbrouwerdigibase <jbrouwer@digibase.nl>
2024-06-20 20:49:04 +0200

    Enable ExportIFCCommonPropertySets

--- BsddRevitPlugin.Logic/UI/Translations/LanguageConverter.cs
+++ BsddRevitPlugin.Logic/UI/Translations/LanguageConverter.cs
@@ -24,14 +24,14 @@ namespace BsddRevitPlugin.Logic.UI.Translations
     }
     public class Dutch
     {
-        public string IFCExport_TaskDialogName { get; } = "IFC Common Property Sets";
-        public string IFCExport_TaskDialogMessage { get; } = "IFC Common Property Sets (PSets) staan aan in de exportinstellingen. Dit kan conflicten geven met de IFC Psets die zijn toegevoegd door de bSDD plugin. Wij adviseren de Revit IFC PSets te deactiveren. \r\r Wil je deze direct uitzetten voor deze export?";
+        // public string IFCExport_TaskDialogName { get; } = "IFC Common Property Sets";
+        // public string IFCExport_TaskDialogMessage { get; } = "IFC Common Property Sets (PSets) staan aan in de exportinstellingen. Dit kan conflicten geven met de IFC Psets die zijn toegevoegd door de bSDD plugin. Wij adviseren de Revit IFC PSets te deactiveren. \r\r Wil je deze direct uitzetten voor deze export?";
     }
 
     public class English
     {
-        public string IFCExport_TaskDialogName { get; } = "IFC Common Property Sets";
-        public string IFCExport_TaskDialogMessage { get; } = "IFC Common Property Sets (PSets) were enabled. This can interfere with the IFC PSets added by the bSDD plugin. We advise you to disable the Revits IFC PSets. \r\r Would you like to disable this right now for this export?";
+        // public string IFCExport_TaskDialogName { get; } = "IFC Common Property Sets";
+        // public string IFCExport_TaskDialogMessage { get; } = "IFC Common Property Sets (PSets) were enabled. This can interfere with the IFC PSets added by the bSDD plugin. We advise you to disable the Revits IFC PSets. \r\r Would you like to disable this right now for this export?";
     }
 
 }
