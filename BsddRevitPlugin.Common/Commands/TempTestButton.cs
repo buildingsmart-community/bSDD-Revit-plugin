76e0b75077cd7476c306a00837260640635b0679
Rien <RvdScheur@DigiBase.nl>
2024-07-16 09:46:15 +0200

    object-type-update (UI v1.2.0!!)

--- BsddRevitPlugin.Common/Commands/TempTestButton.cs
+++ BsddRevitPlugin.Common/Commands/TempTestButton.cs
@@ -1,17 +1,8 @@
 ﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
-using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using Autodesk.Revit.Attributes;
-using Autodesk.Revit.DB;
-using Autodesk.Revit.UI;
 using NLog;
-using BIM.IFC.Export.UI;
-using Autodesk.Revit.DB.IFC;
-using NLog.Fluent;
+using System.Collections.Generic;
 
 namespace BsddRevitPlugin.Common.Commands
 {
@@ -28,53 +19,57 @@ namespace BsddRevitPlugin.Common.Commands
             UIDocument uiDoc = uiApp.ActiveUIDocument;
             Document doc = uiDoc.Document;
 
-            // Toggle the feature state
-            isFeatureEnabled = !isFeatureEnabled;
-
-            // Your code to enable/disable the feature based on isFeatureEnabled
-            // Since we cannot directly toggle UserModifiable, this part would be conceptual.
-            // For example, you might enable/disable a custom handler that intercepts user actions.
-            if (isFeatureEnabled)
+            if (false)
             {
-                // Start a transaction to modify document data
-                using (Transaction tx = new Transaction(doc, "Toggle UserModifiable"))
+                // Toggle the feature state
+                isFeatureEnabled = !isFeatureEnabled;
+
+                // Your code to enable/disable the feature based on isFeatureEnabled
+                // Since we cannot directly toggle UserModifiable, this part would be conceptual.
+                // For example, you might enable/disable a custom handler that intercepts user actions.
+                if (isFeatureEnabled)
                 {
-                    tx.Start();
-
-                    // Iterate through all shared parameters in the document
-                    var bindingMap = doc.ParameterBindings;
-                    DefinitionBindingMapIterator it = bindingMap.ForwardIterator();
-                    while (it.MoveNext())
+                    // Start a transaction to modify document data
+                    using (Transaction tx = new Transaction(doc, "Toggle UserModifiable"))
                     {
-                        if (it.Key is ExternalDefinition externalDefinition &&
-                            externalDefinition.Name.StartsWith("bsdd/"))
-                        {
-                            // Retrieve the current binding for the parameter
-                            Binding binding = bindingMap.get_Item(it.Key);
+                        tx.Start();
 
-                            // Check if the binding is an instance of ElementBinding
-                            if (binding is ElementBinding elementBinding)
+                        // Iterate through all shared parameters in the document
+                        var bindingMap = doc.ParameterBindings;
+                        DefinitionBindingMapIterator it = bindingMap.ForwardIterator();
+                        while (it.MoveNext())
+                        {
+                            if (it.Key is ExternalDefinition externalDefinition &&
+                                externalDefinition.Name.StartsWith("bsdd/"))
                             {
-                                // Toggle UserModifiable for each category in the binding
-                                var categories = elementBinding.Categories;
-                                foreach (Category category in categories)
+                                // Retrieve the current binding for the parameter
+                                Binding binding = bindingMap.get_Item(it.Key);
+
+                                // Check if the binding is an instance of ElementBinding
+                                if (binding is ElementBinding elementBinding)
                                 {
-                                    // Here you would toggle the UserModifiable property
-                                    // Note: Revit API does not directly allow toggling UserModifiable on shared parameters.
-                                    // This step is more about conceptually what you'd want to do, but may not be directly achievable as described.
+                                    // Toggle UserModifiable for each category in the binding
+                                    var categories = elementBinding.Categories;
+                                    foreach (Category category in categories)
+                                    {
+                                        // Here you would toggle the UserModifiable property
+                                        // Note: Revit API does not directly allow toggling UserModifiable on shared parameters.
+                                        // This step is more about conceptually what you'd want to do, but may not be directly achievable as described.
+                                    }
                                 }
                             }
                         }
-                    }
 
-                    tx.Commit();
+                        tx.Commit();
+                    }
+                }
+                else
+                {
+
                 }
             }
-            else
-            {
-               
-            }
-            
+
+
 
             return Result.Succeeded;
         }
