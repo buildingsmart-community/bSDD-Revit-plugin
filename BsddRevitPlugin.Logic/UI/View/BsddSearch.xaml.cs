91610799bcb182c33e3910e3393f20f9fac8feee
Rien <RvdScheur@DigiBase.nl>
2024-08-02 09:56:11 +0200

    fix-revitstairs-in-mappingtable

--- BsddRevitPlugin.Logic/UI/View/BsddSearch.xaml.cs
+++ BsddRevitPlugin.Logic/UI/View/BsddSearch.xaml.cs
@@ -46,11 +46,11 @@ namespace BsddRevitPlugin.Logic.UI.View
 
 
             // Set the address of the CefSharp browser component to the index.html file of the plugin
-            //_browserService.Address = "https://buildingsmart-community.github.io/bSDD-filter-UI/main/bsdd_search/";
+            _browserService.Address = "https://buildingsmart-community.github.io/bSDD-filter-UI/main/bsdd_search/";
             //_browserService.Address = "http://localhost:4173/bsdd_search";
             //_browserService.Address = "http://localhost:3000/bsdd_search";
             //_browserService.Address = "http://localhost:3000/bsdd_search";
-            _browserService.Address = "https://buildingsmart-community.github.io/bSDD-filter-UI/v1.3.1/bsdd_search/";
+            //_browserService.Address = "https://buildingsmart-community.github.io/bSDD-filter-UI/v1.3.1/bsdd_search/";
             var bridgeSearch = new BsddSearchBridge(bsddBridgeData, _bsddLastSelectionEvent);
             bridgeSearch.SetParentWindow(this);
             _browserService.RegisterJsObject("bsddBridge", bridgeSearch, true);
