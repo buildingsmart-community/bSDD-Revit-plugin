1ee4ebdfcafe925255fccb652cde40009f8d9543
Rien <RvdScheur@DigiBase.nl>
2024-11-07 16:28:51 +0100

    add quantities for 4x3

--- BsddRevitPlugin.Logic/UI/View/BsddSelection.xaml.cs
+++ BsddRevitPlugin.Logic/UI/View/BsddSelection.xaml.cs
@@ -77,7 +77,7 @@ namespace BsddRevitPlugin.Logic.UI.View
              //_browserService.Address = "http://localhost:3000/bsdd_selection";
             //_browserService.Address = "https://buildingsmart-community.github.io/bSDD-filter-UI/v1.4.0/bsdd_selection/";
             _browserService.Address = "https://buildingsmart-community.github.io/bSDD-filter-UI/v1.6.0/bsdd_selection/";
-            //_browserService.Address = "https://buildingsmart-community.github.io/bSDD-filter-UI/main/bsdd_selection/";
+            _browserService.Address = "https://buildingsmart-community.github.io/bSDD-filter-UI/main/bsdd_selection/";
             _browserService.RegisterJsObject("bsddBridge", new BsddSelectionBridge(SelectEULS, updateUIEvent), true);
             _browserService.IsBrowserInitializedChanged += OnIsBrowserInitializedChanged;
 
