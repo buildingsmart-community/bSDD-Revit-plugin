35b2ebf1adfc1eb82f7d4038e914e0ffab1cf550
Rien <RvdScheur@DigiBase.nl>
2025-01-21 15:51:21 +0100

    update-null-value-handling

--- BsddRevitPlugin.Logic/Utilities/ParameterDataManagement.cs
+++ BsddRevitPlugin.Logic/Utilities/ParameterDataManagement.cs
@@ -140,6 +140,11 @@ namespace BsddRevitPlugin.Logic.Utilities
                 {
                     foreach (var property in propertySet.HasProperties)
                     {
+
+                        if (property.Name == "Bouwblok")
+                        {
+                            string asfdasdf = "";
+                        }
                         if (property.Type == null)
                         {
                             continue;
@@ -150,27 +155,34 @@ namespace BsddRevitPlugin.Logic.Utilities
                             var propertySingleValue = property as IfcPropertySingleValue;
                             if (propertySingleValue.NominalValue == null)
                             {
-                                continue;
                             }
-                            value = GetParameterValueInCorrectDatatype(propertySingleValue.NominalValue);
-                            specType = GetParameterTypeFromProperty(propertySingleValue.NominalValue);
+                            else
+                            {
+                                value = GetParameterValueInCorrectDatatype(propertySingleValue.NominalValue);
+                                specType = GetParameterTypeFromProperty(propertySingleValue.NominalValue);
+
+                            }
                         }
                         else if (property.Type == "IfcPropertyEnumeratedValue")
                         {
                             var propertyEnumeratedValue = property as IfcPropertyEnumeratedValue;
                             if (propertyEnumeratedValue.EnumerationValues == null || propertyEnumeratedValue.EnumerationValues.Count == 0)
                             {
-                                continue;
                             }
-                            var enumerationValue = propertyEnumeratedValue.EnumerationValues.First();
-                            value = GetParameterValueInCorrectDatatype(enumerationValue);
-                            specType = GetParameterTypeFromProperty(enumerationValue);
+                            else
+                            {
+                                var enumerationValue = propertyEnumeratedValue.EnumerationValues.First();
+                                value = GetParameterValueInCorrectDatatype(enumerationValue);
+                                specType = GetParameterTypeFromProperty(enumerationValue);
+
+                            }
                         }
 
                         //check if instance or type
                         bool isInstance = false;
                         string propertyName = property.Name;
                         string propertySetPropertyName = propertySet.Name + "/" + property.Name;
+
 
                         if (_propertyIsInstanceMap.ContainsKey(propertyName))
                         {
