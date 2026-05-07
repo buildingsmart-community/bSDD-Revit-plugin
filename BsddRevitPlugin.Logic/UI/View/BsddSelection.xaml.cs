013323b8960c6ef40613f3798edb3a3e4526c941
Rien van der Scheur <Rvdscheur@on-track.nl>
2026-05-07 08:23:46 +0200

    Update UI version to v1.8.3 for both search and selection views

--- BsddRevitPlugin.Logic/UI/View/BsddSelection.xaml.cs
+++ BsddRevitPlugin.Logic/UI/View/BsddSelection.xaml.cs
@@ -77,8 +77,8 @@ namespace BsddRevitPlugin.Logic.UI.View
              //_browserService.Address = "http://localhost:3000/bsdd_selection";
             //_browserService.Address = "https://buildingsmart-community.github.io/bSDD-filter-UI/v1.4.0/bsdd_selection/";
             //_browserService.Address = "https://buildingsmart-community.github.io/bSDD-filter-UI/v1.6.0/bsdd_selection/";
-            _browserService.Address = "https://buildingsmart-community.github.io/bSDD-filter-UI/main/bsdd_selection/";
-            _browserService.Address = "https://buildingsmart-community.github.io/bSDD-filter-UI/v1.8.1/bsdd_selection/";
+            //_browserService.Address = "https://buildingsmart-community.github.io/bSDD-filter-UI/main/bsdd_selection/";
+            _browserService.Address = "https://buildingsmart-community.github.io/bSDD-filter-UI/v1.8.3/bsdd_selection/";
             _browserService.RegisterJsObject("bsddBridge", new BsddSelectionBridge(SelectEULS, updateUIEvent), true);
             _browserService.IsBrowserInitializedChanged += OnIsBrowserInitializedChanged;
 
